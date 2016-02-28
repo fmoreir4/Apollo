a1b917673fea71cb3a711305e0fc08bce2176d33
master <fabianomoreira.ti@gmail.com>
2016-02-27 21:17:08 -0300

    textosOK

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-// Empty JS for your own code to be here
+$(function() {
+    //FUNÇÃO PARA ATIVAR O SCROOL SUAVE COM A class="scroll"
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top - 80}, 1000);
+    });
+
+    //FIXA A BARRA DE MENU class="sticky"
+    $(window).scroll(function(){
+        var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();
+
+        if (scroll >= 190)
+  	         sticky.addClass('fixed');
+        else
+  	         sticky.removeClass('fixed');
+    });
+});
